b93de99c98b1b9fe440cc58d941dc683cb2fb133
cng6sk <838928964@qq.com>
2025-09-23 20:13:03 +0800

    init

---
+++ src/main/java/com/cs/csinventory/CsinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.cs.csinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsinventoryApplication.class, args);
+	}
+
+}
